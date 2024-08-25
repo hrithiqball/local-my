8d87624af6d10e5411bb990fc7b171e2842b0256
Harith Iqbal <hrithiqball@gmail.com>
2024-08-25 17:14:59 +0800

    build: bump version

--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Read the version from package.json
 VERSION=$(jq -r '.version' package.json)
+IMAGE_NAME="local/vite-local-my"
 
-# Define the image name
-IMAGE_NAME="vite-local-my"
-
-# Build the Docker image with the version from package.json
 docker build -t ${IMAGE_NAME}:${VERSION} .
 
-# Optional: Print the image name and version for confirmation
 echo "Built Docker image: ${IMAGE_NAME}:${VERSION}"
